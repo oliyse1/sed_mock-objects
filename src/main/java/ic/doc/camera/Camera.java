aae150b2c753e71a522e630e43ad5409e596cc71
sy3111 <sy3111@ic.ac.uk>
2022-02-08 15:02:15 +0000

    added test switchingTheCameraOnPowersUpTheSensor()

--- src/main/java/ic/doc/camera/Camera.java
+++ src/main/java/ic/doc/camera/Camera.java
@@ -2,12 +2,17 @@ package ic.doc.camera;
 
 public class Camera {
 
+  private Sensor sensor;
+
+  Camera(Sensor sensor) {
+    this.sensor = sensor;
+  }
   public void pressShutter() {
     // not implemented
   }
 
   public void powerOn() {
-    // not implemented
+    sensor.powerUp();
   }
 
   public void powerOff() {
--- src/test/java/ic/doc/camera/CameraTest.java
+++ src/test/java/ic/doc/camera/CameraTest.java
@@ -1,18 +1,32 @@
 package ic.doc.camera;
 
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 public class CameraTest {
 
   @Rule
   public JUnitRuleMockery context = new JUnitRuleMockery();
 
+
+
+  Sensor sensor = context.mock(Sensor.class);
+  Camera camera = new Camera(sensor);
+
   @Test
   public void switchingTheCameraOnPowersUpTheSensor() {
 
-    // write your test here
+    context.checking(new Expectations() {{
+      exactly(1).of(sensor).powerUp();
+    }});
+
+    camera.powerOn();
+
   }
 
 }
