3a15e25918ec28910ddf37722926b62fe449d2aa
sy3111 <sy3111@ic.ac.uk>
2022-02-10 12:25:20 +0000

    cleaned up comments and formatting

--- src/main/java/ic/doc/camera/Camera.java
+++ src/main/java/ic/doc/camera/Camera.java
@@ -1,42 +1,65 @@
 package ic.doc.camera;
 
 public class Camera implements WriteListener {
-  //assume camera is off by default
+
+  // Field to store whether camera power is on, assume camera is off by default
   private boolean powerOn = false;
-  private MemoryCard memoryCard;
-  private Sensor sensor;
-  //assumed no data is currently being written as default
+
+  // Field to store whether data is currently being written, assumed no data is currently being
+  // written as default
   private boolean isWriting = false;
 
-  Camera(Sensor sensor, MemoryCard memoryCard) {
+  // Other class members
+  private MemoryCard memoryCard;
+  private Sensor sensor;
+
+  // Constructor for Camera class
+  public Camera(Sensor sensor, MemoryCard memoryCard) {
     this.sensor = sensor;
     this.memoryCard = memoryCard;
   }
+
+  // Function that mimics behaviour of pressing the shutter on a camera
   public void pressShutter() {
-    if(powerOn) {
+    // If camera power is on, pressing the shutter copies data from the sensor to the memory card
+    if (powerOn) {
       byte[] data = sensor.readData();
       memoryCard.write(data);
+      // Set isWriting status to true, starts writing
       isWriting = true;
     }
   }
 
+  // Function that turns the power of the camera on
+  // The coursework spec does not say what happens to the sensor when a second image is taken
+  // without turning the power off and on again after taking the first image, because the sensor
+  // powers down after the first image has been written to the memory card
+  // Therefore i've assumed that powerOn is called each time before taking a new image to power up
+  // the sensor
   public void powerOn() {
     powerOn = true;
     sensor.powerUp();
   }
 
+  // Function that turns the power of the camera off
   public void powerOff() {
 
     powerOn = false;
+    // Turning the camera off only powers down the sensor when data is not currently being written
     if (isWriting == false) {
       sensor.powerDown();
     }
   }
 
+  // Overridden function of the WriteListener interface
+  // This function can be called by other components to let the camera know the status of writing
+  // data
+  // This function sets isWriting status to false and powers down the sensor when writing is
+  // completed
   @Override
   public void writeComplete() {
+
     isWriting = false;
     sensor.powerDown();
   }
 }
-
--- src/test/java/ic/doc/camera/CameraTest.java
+++ src/test/java/ic/doc/camera/CameraTest.java
@@ -1,97 +1,123 @@
 package ic.doc.camera;
 
 import org.jmock.Expectations;
-import org.jmock.Mockery;
-import org.jmock.integration.junit4.JMock;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 public class CameraTest {
+  // Create rule to manage JMock expectations
+  @Rule public JUnitRuleMockery context = new JUnitRuleMockery();
 
-  @Rule
-  public JUnitRuleMockery context = new JUnitRuleMockery();
-
-
-
+  // Create mock objects
   Sensor sensor = context.mock(Sensor.class);
   MemoryCard memoryCard = context.mock(MemoryCard.class);
 
+  // Instantiate the camera
   Camera camera = new Camera(sensor, memoryCard);
   WriteListener writeListener = camera;
 
-  byte DATA[] = {20,10,30,5};
+  // Create mock data that is passed from sensor to memory card when the shutter is pressed while
+  // camera is on
+  static final byte[] DATA = {20, 10, 30, 5, 25};
 
-
+  // Test to check that switching the camera on powers up the sensor
   @Test
   public void switchingTheCameraOnPowersUpTheSensor() {
 
-    context.checking(new Expectations() {{
-      exactly(1).of(sensor).powerUp();
-    }});
+    context.checking(
+        new Expectations() {
+          {
+            exactly(1).of(sensor).powerUp();
+          }
+        });
 
     camera.powerOn();
   }
 
+  // Test to check that switching the camera off powers down the sensor
   @Test
   public void switchingTheCameraOffPowersDownTheSensor() {
 
-    context.checking(new Expectations() {{
-      exactly(1).of(sensor).powerDown();
-    }});
+    context.checking(
+        new Expectations() {
+          {
+            exactly(1).of(sensor).powerDown();
+          }
+        });
 
     camera.powerOff();
   }
 
+  // Test to check that pressing the shutter when the camera power is off does nothing
   @Test
   public void pressingTheShutterWhenThePowerIsOffDoesNothing() {
 
-    context.checking(new Expectations() {{
-      exactly(1).of(sensor).powerDown();
-    }});
+    context.checking(
+        new Expectations() {
+          {
+          }
+        });
 
-    camera.powerOff();
+    // Default state of camera is off
     camera.pressShutter();
   }
 
+  // Test to check that data is copied from the sensor to memory card
+  // when the shutter is pressed with the camera power on
   @Test
   public void pressingTheShutterWithThePowerOnCopiesDataFromTheSensorToMemoryCard() {
 
-    context.checking(new Expectations() {{
-      exactly(1).of(sensor).powerUp();
-      exactly(1).of(sensor).readData(); will(returnValue(DATA));
-      exactly(1).of(memoryCard).write(DATA);
-    }});
+    context.checking(
+        new Expectations() {
+          {
+            exactly(1).of(sensor).powerUp();
+            exactly(1).of(sensor).readData();
+            will(returnValue(DATA));
+            exactly(1).of(memoryCard).write(DATA);
+          }
+        });
+
     camera.powerOn();
     camera.pressShutter();
   }
 
+  // Test to check that if data is currently being written, switching the camera off does not power
+  // down the sensor
   @Test
   public void switchingTheCameraOffWhileWritingDoesNotPowerDownTheSensor() {
 
-    context.checking(new Expectations() {{
-      exactly(1).of(sensor).powerUp();
-      exactly(1).of(sensor).readData(); will(returnValue(DATA));
-      exactly(1).of(memoryCard).write(DATA);
-    }});
+    context.checking(
+        new Expectations() {
+          {
+            exactly(1).of(sensor).powerUp();
+            exactly(1).of(sensor).readData();
+            will(returnValue(DATA));
+            exactly(1).of(memoryCard).write(DATA);
+            // no calls to powerDown expected
+          }
+        });
     camera.powerOn();
     camera.pressShutter();
     camera.powerOff();
   }
 
+  // Test to check that once writing the data has completed, the camera powers down the sensor
   @Test
   public void cameraPowersDownTheSensorOnceWritingHasCompleted() {
 
-    context.checking(new Expectations() {{
-      exactly(1).of(sensor).powerUp();
-      exactly(1).of(sensor).readData(); will(returnValue(DATA));
-      exactly(1).of(memoryCard).write(DATA);
-      exactly(1).of(sensor).powerDown();
-    }});
+    context.checking(
+        new Expectations() {
+          {
+            exactly(1).of(sensor).powerUp();
+            exactly(1).of(sensor).readData();
+            will(returnValue(DATA));
+            exactly(1).of(memoryCard).write(DATA);
+            exactly(1).of(sensor).powerDown();
+          }
+        });
     camera.powerOn();
     camera.pressShutter();
     writeListener.writeComplete();
   }
-
 }
